f7c7925032b303a65e850cc28a080df124c6ee96
Aleja Duque-Torres <duquet@ut.ee>
2023-02-23 09:52:15 +0200

    some test

--- AuxExp/MethodsOneInput/add_values.py
+++ AuxExp/MethodsOneInput/add_values.py
@@ -4,3 +4,23 @@ def add_values(data):
     for i in data:
         sum += i
     return sum
+
+# if __name__ == '__main__':
+
+#     import click 
+
+#     global mainDF
+#     mainDF = {}
+
+#     global auxListmainDF
+#     auxListmainDF = []
+
+#     @click.command()
+#     @click.option('-i', '--input_path', 'input_path')
+
+#     def main(input_path):
+
+        
+        
+        
+# main()
